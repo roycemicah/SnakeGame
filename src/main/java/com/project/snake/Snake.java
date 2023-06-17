909fac24ee237e9b50d9a02da3435a72fb0392da
roycemicah <royce.rabanal93@gmail.com>
2023-06-17 21:11:38 +0800

    First push, full working snake game

---
+++ src/main/java/com/project/snake/Snake.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.project.snake;
+
+/**
+ *
+ * @author roycerabanal
+ */
+public class Snake {
+
+    public static void main(String[] args) {
+        new GameFrame();
+    }
+
+}
